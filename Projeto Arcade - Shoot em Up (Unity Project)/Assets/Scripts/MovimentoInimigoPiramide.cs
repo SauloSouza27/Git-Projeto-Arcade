2e663da2bb107feade1866f0e908d0323aa8f2e3
saulosouza2703 <126815810+saulosouza2703@users.noreply.github.com>
2023-04-10 22:02:20 -0300

    Correçoes piramide

--- Projeto Arcade - Shoot em Up (Unity Project)/Assets/Scripts/MovimentoInimigoPiramide.cs
+++ Projeto Arcade - Shoot em Up (Unity Project)/Assets/Scripts/MovimentoInimigoPiramide.cs
@@ -120,6 +120,7 @@ public class MovimentoInimigoPiramide : MonoBehaviour
         transform.up = Vector3.Slerp(transform.up, -1 * direcao, velocidadeRotacao * Time.deltaTime);
         // Mira cabeca
         cabecaPiramide.transform.up = Vector3.Slerp(cabecaPiramide.transform.up, -1 * direcao, 3 * velocidadeRotacao * Time.deltaTime);
+        //cabecaPiramide.transform.rotation = Quaternion.LookRotation(cabecaPiramide.transform.forward, direcao);
     }
 
     // Tiro
