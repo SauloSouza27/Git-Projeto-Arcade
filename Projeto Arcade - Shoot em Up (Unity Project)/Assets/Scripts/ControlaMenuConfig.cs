809fa9bcf27a886b11eb27bf4db46bcb6bcfb417
saulosouza2703 <126815810+saulosouza2703@users.noreply.github.com>
2023-04-11 22:03:10 -0300

    Botão Config Programação

---
+++ Projeto Arcade - Shoot em Up (Unity Project)/Assets/Scripts/ControlaMenuConfig.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControlaMenuConfig : MonoBehaviour
+{
+    public void CarregaCena(string nomeCena)
+    {
+        SceneManager.LoadScene(nomeCena);
+    }
+
+    public void FecharJogo()
+    {
+        Application.Quit();
+    }
+
+    public void PausaJogo(bool pausa)
+    {
+        if (pausa)
+        {
+            Time.timeScale = 0;
+        }
+        else Time.timeScale = 1;
+    }
+}
