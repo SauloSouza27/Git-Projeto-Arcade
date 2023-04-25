d5c7f9b4b2012ebba280ed3ee251b6b7024fb57e
saulosouza2703 <126815810+saulosouza2703@users.noreply.github.com>
2023-04-25 14:48:54 -0300

    Animaçao boss

--- Projeto Arcade - Shoot em Up (Unity Project)/Assets/Scripts/MovimentoBoss.cs
+++ Projeto Arcade - Shoot em Up (Unity Project)/Assets/Scripts/MovimentoBoss.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class MovimentoBoss : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private GameObject alvo, controladorGame;
+    public GameObject cabecaPiramide, corpoPiramide;
+    // Controle rotaçao
+    public float velocidadeRotacao = 2.0f;
+    // Materiais
+    MeshRenderer[] renderers;
+    Material[] materiais;
+    private void Awake()
     {
-        
+        controladorGame = GameObject.FindGameObjectWithTag("ControladorGame");
+        alvo = GameObject.FindGameObjectWithTag("Player");
+        // Busca materiais do inimigo
+        renderers = GetComponentsInChildren<MeshRenderer>();
+        materiais = new Material[renderers.Length];
+        for (int i = 0; i < renderers.Length; i++)
+        {
+            materiais[i] = renderers[i].material;
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Time.timeScale == 0) return;
+
+        MovimentaBossPiramide();
+    }
+    private void MovimentaBossPiramide()
+    {
+        // Rotacao corpo
+        Vector3 direcao = alvo.transform.position - transform.position;
+        direcao = direcao.normalized;
+        corpoPiramide.transform.up = Vector3.Slerp(corpoPiramide.transform.up, - direcao, velocidadeRotacao * Time.deltaTime);
+        // Mira cabeca
+        //cabecaPiramide.transform.up = Vector3.Slerp(cabecaPiramide.transform.up, -1 * direcao, 3 * velocidadeRotacao * Time.deltaTime);
+        cabecaPiramide.transform.rotation = Quaternion.LookRotation(cabecaPiramide.transform.forward, - direcao);
     }
 }
