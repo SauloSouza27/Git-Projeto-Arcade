7879233fe59dcbad1cd9e1168674029c186947e6
saulosouza2703 <126815810+saulosouza2703@users.noreply.github.com>
2023-05-24 20:20:54 -0300

    fase2

--- Projeto Arcade - Shoot em Up (Unity Project)/Assets/Scripts/Inimigos/SpawnAnubis.cs
+++ Projeto Arcade - Shoot em Up (Unity Project)/Assets/Scripts/Inimigos/SpawnAnubis.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class SpawnAnubis : MonoBehaviour
 {
     private float contadorCooldown;
-    public float atrasaSpawn = 0.0f, cooldownSpawnAnubis = 2.0f, velocidadeMovimento = 6.0f;
-    public int quantidadeParaSpawnar = 3;
+    public float atrasaSpawn = 0.0f, cooldownSpawnAnubis = 2.0f, velocidadeMovimento = 4.0f;
+    public int quantidadeParaSpawnar = 2, pontosVida = 20;
+    public float atrasaDisparos = 0.0f, velocidadeProjetil = 40.0f;
     private int contador;
     public GameObject anubisPrefab;
     public bool ativar = true;
@@ -26,8 +27,12 @@ public class SpawnAnubis : MonoBehaviour
         if (contadorCooldown == 0 && ativar == true && contador < quantidadeParaSpawnar)
         {
             GameObject instancia = Instantiate(anubisPrefab, transform.position, transform.rotation);
-            MovimentoInimigoEspinhoso status = instancia.GetComponent<MovimentoInimigoEspinhoso>();
+            MovimentoAnubis status = instancia.GetComponent<MovimentoAnubis>();
+            status.atrasaDisparos = atrasaDisparos;
+            status.velocidadeProjetil = velocidadeProjetil;
             status.velocidadeMovimento = velocidadeMovimento;
+            status.pontosVida = pontosVida;
+            if (instancia != null) return;
             contadorCooldown = cooldownSpawnAnubis;
             contador++;
         }
