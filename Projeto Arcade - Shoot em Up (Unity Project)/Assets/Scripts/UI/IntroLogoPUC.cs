d7e7be2ba3aedd3d900e3e20f3b3cb68c99c25c5
saulosouza2703 <126815810+saulosouza2703@users.noreply.github.com>
2023-06-14 16:42:02 -0300

    intro log puc

--- Projeto Arcade - Shoot em Up (Unity Project)/Assets/Scripts/UI/IntroLogoPUC.cs
+++ Projeto Arcade - Shoot em Up (Unity Project)/Assets/Scripts/UI/IntroLogoPUC.cs
@@ -2,15 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class IntroLogoPUC : MonoBehaviour
 {
 
-    public float timeLoadScene = 4.0f;
+    public float timeLoadScene = 5.0f;
     private float timer;
+
+    public float tempoFade = 1.0f;
+    public Image logo;
+    private bool fadeIn = true;
 
     private void Update()
     {
+        FadeInOut();
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             CarregaCena("Menu Inicial");
@@ -21,9 +28,35 @@ public class IntroLogoPUC : MonoBehaviour
         CarregaCena("Menu Inicial");
     }
 
-    public void CarregaCena(string nomeCena)
+    private void CarregaCena(string nomeCena)
     {
         Time.timeScale = 1;
         SceneManager.LoadScene(nomeCena);
+    }
+
+    private void FadeInOut()
+    {
+        if (fadeIn)
+        {
+            for (float i = 0; i <= 1; i += Time.deltaTime * tempoFade)
+            {
+                logo.color = new Color(1, 1, 1, i);
+                if(i == 1)
+                {
+                    fadeIn = false;
+                }
+            }
+        }
+        else
+        {
+            for (float i = 1; i >= 0; i -= Time.deltaTime * tempoFade)
+            {
+                logo.color = new Color(1, 1, 1, i);
+                if (i == 0)
+                {
+                    fadeIn = true;
+                }
+            }
+        }
     }
 }
